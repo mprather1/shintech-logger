e73cf8b1f85a3d285a2f3b56b046fbb7d4b0c4ff
Michael Prather <mprather1@gmail.com>
2018-11-24 22:13:32 +0000

    NODE_ENV

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const winston = require('winston')
 const path = require('path')
+const NODE_ENV = process.env['NODE_ENV']
 
-module.exports = function ({ environment = 'development' }) {
-  if (environment === 'test') {
+module.exports = function () {
+  if (NODE_ENV === 'test') {
     return winston.createLogger({
       transports: [
         new winston.transports.File({ filename: path.join('log', 'test.log'), level: 'warn' })
@@ -20,7 +21,7 @@ module.exports = function ({ environment = 'development' }) {
     ]
   })
 
-  if (environment !== 'production') {
+  if (NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
       format: winston.format.combine(
         winston.format.colorize(),
